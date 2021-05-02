2b507d57db29bf71c903a2a078cbeaf966ff67aa
Cody Scott <cs475x@icloud.com>
2021-05-01 23:34:07 -0700

    Update Tailwind, enable JIT, and add member count to Discord block

--- src/components/Discord.vue
+++ src/components/Discord.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex flex-wrap items-center justify-center sm:px-8 px-4 pb-6">
-    <p class="text-white font-medium md:text-2xl sm:text-lg text-sm mr-2 -mt-1">Play with others &amp; stay up to date on</p>
+    <p class="text-white font-medium md:text-2xl sm:text-lg text-sm mr-2 -mt-1">
+      Play with
+      <span v-if="memberCount > 0" class="font-black underline">{{ new Intl.NumberFormat().format(memberCount) }}</span>
+      others &amp; stay up to date on</p>
     <a href="https://discord.gg/polus" target="_blank" class="block">
       <img class="object-contain sm:h-14 h-8" alt="Discord logo" src="https://static.polus.gg/images/logos/discord/discord_white.svg">
     </a>
@@ -12,5 +15,25 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "Discord",
+  data() {
+    return {
+      memberCount: 0,
+    };
+  },
+  beforeMount() {
+    this.getMemberCount();
+  },
+  methods: {
+    async getMemberCount() {
+      try {
+        const res = await fetch("https://discord.com/api/v8/invites/polus?with_counts=true");
+        const data = await res.json();
+
+        this.memberCount = data["approximate_member_count"] ?? 0;
+      } catch (error) {
+        console.error("Error fetching Discord invite details", error);
+      }
+    },
+  },
 });
 </script>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -17,7 +17,7 @@
           </div>
           <div class="hidden sm:block sm:ml-6">
             <div class="flex space-x-4">
-              <a href="/" class="bg-purple-900 text-white px-3 py-2 rounded-md text-sm font-medium">Homepage</a>
+              <a href="/" class="bg-[#5f3f72] text-white px-3 py-2 rounded-md text-sm font-medium">Homepage</a>
               <a class="cursor-not-allowed text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Leaderboards</a>
               <a class="cursor-not-allowed text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
               <a class="cursor-not-allowed text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Staff</a>
@@ -28,7 +28,7 @@
     </div>
     <div v-if="menuOpen" class="sm:hidden" id="mobile-menu">
       <div class="px-2 pt-2 pb-3 space-y-1">
-        <a href="/" class="bg-purple-900 text-white block px-3 py-2 rounded-md text-base font-medium">Homepage</a>
+        <a href="/" class="bg-[#5f3f72] text-white block px-3 py-2 rounded-md text-base font-medium">Homepage</a>
         <a class="cursor-not-allowed text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Leaderboards</a>
         <a class="cursor-not-allowed text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contact</a>
         <a class="cursor-not-allowed text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Staff</a>
