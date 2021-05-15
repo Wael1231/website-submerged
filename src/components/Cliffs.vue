8fd5d69301afbab5aa25b6f50a3153396ec42db9
Cody Scott <cs475x@icloud.com>
2021-05-14 20:20:41 -0700

    Tiling cliffs

--- src/components/Cliffs.vue
+++ src/components/Cliffs.vue
@@ -1,9 +1,7 @@
 <template>
-  <img class="object-cover"
-       width="1920"
-       height="406"
-       src="https://static.polus.gg/images/cliffs.png"
-       alt="Footer cliffs">
+  <div class="bg-repeat-x bg-local bg-contain lg:h-96 sm:h-80 h-64 bg-bottom lg:min-h-96 sm:min-h-80 min-h-64"
+       style="background-image: url('https://static.polus.gg/images/cliffs_tile.png')"
+  />
 </template>
 
 <script lang="ts">
@@ -13,3 +11,21 @@ export default defineComponent({
   name: "Cliffs",
 });
 </script>
+
+<style>
+.min-h-64 {
+  min-height: 16rem;
+}
+
+@media (min-width: 640px) {
+  .sm\:min-h-80 {
+    min-height: 20rem;
+  }
+}
+
+@media  (min-width: 1024px) {
+  .lg\:min-h-96 {
+    min-height: 24rem;
+  }
+}
+</style>
